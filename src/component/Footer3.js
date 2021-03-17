82a00fc75824294949c67fee3c9fb9066cd43f2a
santhosh162000 <santhosh.m@codingmart.com>
2021-03-17 16:59:47 +0530

    santhosh

--- src/component/Footer3.js
+++ src/component/Footer3.js
@@ -8,19 +8,19 @@ const Footer3 = () => {
       <div className="about-container1">
         <h5>Most Searched Brands on Shoppersstop.com</h5>
         <p>Watches: <a href="#/">FOSSIL</a> | <a href="#/">TITAN</a> | <a href="#/">MK</a> | <a href="#/">CASIO</a> | <a href="#/">TOMMY</a> | <a href="#/">ARMANI EXCHANGE</a> | <a href="#/">EMPORIO ARMANI</a> | <a href="#/">GUESS</a> | <a href="#/">TIMEX</a> | <a href="#/">GIORDANA</a> | <a href="#/">CITIZEN</a> | <a href="#/">DANIEL WELLINGTON</a> | <a href="#/">VERSUS</a> | <a href="#/">TISSOT</a> | <a href="#/">CERRUTI</a></p>
-        
+        <br></br>
         <p>Apparel: <a href="#/">AND</a> | <a href="#/">VERMODA</a> | <a href="#/">U.S POLO ASSN</a> | <a href="#/">TOMMY HILFIGER</a> | <a href="#/">STOP</a> | <a href="#/">CALVIN KLEIN JEANS</a> | <a href="#/">JACK & JONES</a> | <a href="#/">MOTHERCARE</a> | <a href="#/">HAUTE CURRY</a> | <a href="#/">W</a> | <a href="#/">ZINK LONDON</a></p>
-
+        <br></br>
         <p>Shoes: <a href="#/">WOODLAND SHOES</a> | <a href="#/">REDTAPE SHOES</a> | <a href="#/">CLARK SHOES</a> | <a href="#/">INC.5</a> | <a href="#/">CATWALK</a> | <a href="#/">STEVE MADDEN</a> | <a href="#/">REEBOK</a> | <a href="#/">TRESMODE</a> | <a href="#/">PUMA</a> | <a href="#/">ADIDAS</a> </p>
-
+        <br></br>
         <p>Handbags: <a href="#/">LAVIE</a> | <a href="#/">HIDESIGN</a> | <a href="#/">BAGGIT</a> | <a href="#/">CAPRESE</a> | <a href="#/">HOLII</a> | <a href="#/">GIORDANO</a> </p>
-
+        <br></br>
         <p>Beauty: <a href="#/">LAKME</a> | <a href="#/">L'OREAL</a> | <a href="#/">MAYBELLINE</a> | <a href="#/">REVLON</a> | <a href="#/">COLORBAR</a> | <a href="#/">MAC</a> | <a href="#/">CLINIQUE</a> | <a href="#/">ESTEE LAUDER</a> | <a href="#/">BOBBI BROWN</a> | <a href="#/">BURBERRY</a> </p>
-
+        <br></br>
         <p>Home: <a href="#/">PHILIPS</a> | <a href="#/">WONDERCHEF</a> | <a href="#/">FERN</a> | <a href="#/">TREASURES</a> | <a href="#/">SPACES</a> | <a href="#/">IVY</a> | <a href="#/">DEVON NORTH</a> | <a href="#/">MASPAR</a> | <a href="#/">SPREAD</a> | <a href="#/">MEYER</a> | <a href="#/">LIVING WORLD</a> | <a href="#/">BACK TO EARTH</a> </p>
-
+        <br></br> 
         <p>Sunglasses: <a href="#/">OPIUM</a> | <a href="#/">TOMMY HILFIGER</a> | <a href="#/">CARRERA</a> | <a href="#/">IDEE</a> | <a href="#/">CK</a> | <a href="#/">RAYBAN</a> | <a href="#/">OAKLAY</a> | <a href="#/">VOGUE</a> | <a href="#/">SCOTT</a> | <a href="#/">FCUK</a> | <a href="#/">POLAROID</a> | <a href="#/">VELOCITY</a> | <a href="#/">TOM FORD</a> | <a href="#/">BEBE</a> | <a href="#/">GIO COLLECTION</a> </p>
-
+        <br></br>
         <p>Jewellery: <a href="#/">SHAZE</a> | <a href="#/">VOYLLA</a> | <a href="#/">SWAROVSKI</a> | <a href="#/">YELLOW CHIMES</a> | <a href="#/">FOSSIL</a> | <a href="#/">AYESHA</a> | <a href="#/">SKAGEN</a> | <a href="#/">PRETTY WOMEN</a> | <a href="#/">ARMANI</a> | <a href="#/">MICHAEL CHORS</a> | <a href="#/">ACCESSORIZE</a> | <a href="#/">DANIEL WELLINGTON</a> | <a href="#/">TRIBAL ZONE</a> | <a href="#/">REAL EFFECT</a> </p>
       
       </div>
